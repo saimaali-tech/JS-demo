7b1aa550596ac76ca31bedba6e93121032124394
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-30 21:20:25 +0500

    covered all datatypes

--- datatypes.js
+++ datatypes.js
@@ -4,3 +4,21 @@ let price = 199.99; // decimal
 
 console.log(age);   // 22
 console.log(price); // 199.99
+
+let name = "Saima";
+let course = 'JavaScript';
+
+console.log(name);
+console.log(course);
+
+let role = "DevOps Intern";
+console.log(`My role is ${role}`);
+
+console.log(typeof 10);         // number
+console.log(typeof "Hello");    // string
+console.log(typeof true);       // boolean
+console.log(typeof undefined);  // undefined
+console.log(typeof null);       // object ❌ (JS bug)
+console.log(typeof []);         // object
+console.log(typeof {});         // object
+console.log(typeof function(){}); // function
