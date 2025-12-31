7edcd94c73d0f776eb9e03175e7d7c96a321488a
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-31 14:44:40 +0500

    for loop

--- loops.js
+++ loops.js
@@ -1,3 +1,8 @@
 // A loop repeatedly executes a block of code while a condition is true.
 //This avoids repeating code manually.
 
+//for Loop
+
+for (let i = 1; i <= 5; i++) {
+  console.log("Count: " + i);
+}
