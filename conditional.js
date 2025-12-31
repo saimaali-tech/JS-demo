64e11e00316a78cb73811bde658947f0936cf6b2
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-31 14:37:41 +0500

    covered all conditional statements

--- conditional.js
+++ conditional.js
@@ -1,10 +1,10 @@
 // //if Statement
 
-// let age = 20;
+let age = 20;
 
-// if (age >= 18) {
-//   console.log("You are eligible to vote");
-// }
+if (age >= 18) {
+  console.log("You are eligible to vote");
+}
 
 
 // // if...else
@@ -20,26 +20,26 @@
 
 // let marks = 75;
 
-// if (marks >= 90) {
-//   console.log("Grade A");
-// } else if (marks >= 70) {
-//   console.log("Grade B");
-// } else if (marks >= 50) {
-//   console.log("Grade C");
-// } else {
-//   console.log("Fail");
-// }
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 70) {
+  console.log("Grade B");
+} else if (marks >= 50) {
+  console.log("Grade C");
+} else {
+  console.log("Fail");
+}
 
 //switch Statement
 
-// switch(expression) {
-//   case value1:
-//     // code
-//     break;
-//   case value2:
-//     // code
-//     break;
-//   default:
-//     // code
-// }
+switch(expression) {
+  case value1:
+    // code
+    break;
+  case value2:
+    // code
+    break;
+  default:
+    // code
+}
 
