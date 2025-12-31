e37a1868c01fa642e6f59495eaa1d9e560f99343
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-31 14:33:55 +0500

    if-else-if statement

--- conditional.js
+++ conditional.js
@@ -7,11 +7,27 @@
 // }
 
 
-// if...else
-    let age = 16;
+// // if...else
+//     let age = 16;
 
-if (age >= 18) {
-  console.log("You are an adult");
+// if (age >= 18) {
+//   console.log("You are an adult");
+// } else {
+//   console.log("You are a minor");
+// }
+
+//if...else if...else
+
+let marks = 75;
+
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 70) {
+  console.log("Grade B");
+} else if (marks >= 50) {
+  console.log("Grade C");
 } else {
-  console.log("You are a minor");
+  console.log("Fail");
 }
+
+
