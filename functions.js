2708f1725b25002a5e616be8c401e5aac7719aa7
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-01 21:44:45 +0500

    functions with parameters

--- functions.js
+++ functions.js
@@ -1,7 +1,15 @@
 //A function is a reusable block of code that performs a task.
 
-function greet() {
-  console.log("Hello World");
+// function greet() {
+//   console.log("Hello World");
+// }
+
+// greet(); // calling the function
+
+//Function with Parameters
+function greet(name) {
+  console.log("Hello " + name);
 }
 
-greet(); // calling the function
+greet("Saima");
+greet("Ali");
