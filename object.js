265b3b48ad41dc1f6eac3f268677f083f7d8ae35
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-01 21:34:00 +0500

    some updates in ./object.js

--- object.js
+++ object.js
@@ -6,3 +6,10 @@ const user = {
 };
 
 console.log(user);
+console.log(user.name); // Saima
+console.log(user.role); // Web Developer Intern
+user.role = "DevOps Intern"; // update
+user.city = "Lahore";       // add new property
+
+console.log(user);
+
