e1c3c0fd7d82846e14954c0f4d2e4d2953d1ee9f
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-01 21:47:31 +0500

    function expression

--- functions.js
+++ functions.js
@@ -15,9 +15,18 @@
 // greet("Ali");
 
 //Function with Return Valu
-function add(a, b) {
-  return a + b;
-}
+// function add(a, b) {
+//   return a + b;
+// }
 
-let result = add(5, 3);
-console.log(result); // 8
+// let result = add(5, 3);
+// console.log(result); // 8
+
+//Function Expression
+const multiply = function (a, b) {
+  return a * b;
+};
+
+console.log(multiply(4, 2));
+
+
