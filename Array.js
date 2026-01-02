a5667a7a235352232ebeb96299cc3118b44876f8
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-02 11:28:11 +0500

    accessing array elements

--- Array.js
+++ Array.js
@@ -1,4 +1,12 @@
 // An array is a data structure that stores multiple values in a single variable.
 
+// const fruits = ["Apple", "Banana", "Mango"];
+// console.log(fruits);
+
+// Accessing Array Elements
+
 const fruits = ["Apple", "Banana", "Mango"];
-console.log(fruits);
+
+console.log(fruits[0]); // Apple
+console.log(fruits[1]); // Banana
+console.log(fruits[2]); // Mango
