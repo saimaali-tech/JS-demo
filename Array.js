8fda22e1b7fcb0132942e79e80e1369746a1f1f1
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-02 11:29:15 +0500

    accessing array elements

--- Array.js
+++ Array.js
@@ -10,3 +10,8 @@ const fruits = ["Apple", "Banana", "Mango"];
 console.log(fruits[0]); // Apple
 console.log(fruits[1]); // Banana
 console.log(fruits[2]); // Mango
+
+console.log(fruits.length); // 3
+fruits[1] = "Orange";
+console.log(fruits); // ["Apple", "Orange", "Mango"]
+
