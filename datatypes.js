1ff8b783ece40634a5d4a0cd02df4530af647f20
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-03 13:14:35 +0500

    non-primitive data types

--- datatypes.js
+++ datatypes.js
@@ -1,29 +1,40 @@
 
 
-let age = 22;        // integer
-let price = 199.99; // decimal
+// let age = 22;        // integer
+// let price = 199.99; // decimal
 
-console.log(age);   // 22
-console.log(price); // 199.99
+// console.log(age);   // 22
+// console.log(price); // 199.99
 
-let bigNumber = 123456789012345678901234567890n;
-console.log(bigNumber);
+// let bigNumber = 123456789012345678901234567890n;
+// console.log(bigNumber);
 
 
-let name = "Saima";
-let course = 'JavaScript';
+// let name = "Saima";
+// let course = 'JavaScript';
 
-console.log(name);
-console.log(course);
+// console.log(name);
+// console.log(course);
 
-let role = "DevOps Intern";
-console.log(`My role is ${role}`);
+// let role = "DevOps Intern";
+// console.log(`My role is ${role}`);
 
-console.log(typeof 10);         // number
-console.log(typeof "Hello");    // string
-console.log(typeof true);       // boolean
-console.log(typeof undefined);  // undefined
-console.log(typeof null);       // object ❌ (JS bug)
-console.log(typeof []);         // object
-console.log(typeof {});         // object
-console.log(typeof function(){}); // function
+// console.log(typeof 10);         // number
+// console.log(typeof "Hello");    // string
+// console.log(typeof true);       // boolean
+// console.log(typeof undefined);  // undefined
+// console.log(typeof null);       // object ❌ (JS bug)
+// console.log(typeof []);         // object
+// console.log(typeof {});         // object
+// console.log(typeof function(){}); // function
+
+//Non-Primitive (Reference) Data Types
+let user = {
+  name: "Saima",
+  age: 22,
+  role: "Web Developer Intern"
+};
+
+// Access values
+console.log(user.name);
+console.log(user.role);
