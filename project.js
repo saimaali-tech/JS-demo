abebe3c26f62dc66f47fba4561d90bcef149c627
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-03 13:09:41 +0500

    mini js project 2

--- project.js
+++ project.js
@@ -1,23 +1,42 @@
-const todos = [];
+// const todos = [];
 
-function addTodo(task) {
-  todos.push({ task, completed: false });
+// function addTodo(task) {
+//   todos.push({ task, completed: false });
+// }
+
+// function completeTodo(index) {
+//   todos[index].completed = true;
+// }
+
+// function showTodos() {
+//   todos.forEach((todo, i) => {
+//     console.log(
+//       `${i + 1}. ${todo.task} - ${todo.completed ? "Done" : "Pending"}`
+//     );
+//   });
+// }
+
+// // Test
+// addTodo("Learn JavaScript");
+// addTodo("Practice Arrays");
+// completeTodo(0);
+// showTodos();
+
+
+function calculate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return b !== 0 ? a / b : "Cannot divide by zero";
+    default:
+      return "Invalid operator";
+  }
 }
 
-function completeTodo(index) {
-  todos[index].completed = true;
-}
-
-function showTodos() {
-  todos.forEach((todo, i) => {
-    console.log(
-      `${i + 1}. ${todo.task} - ${todo.completed ? "Done" : "Pending"}`
-    );
-  });
-}
-
-// Test
-addTodo("Learn JavaScript");
-addTodo("Practice Arrays");
-completeTodo(0);
-showTodos();
+console.log(calculate(10, 5, "+"));
+console.log(calculate(10, 0, "/"));
