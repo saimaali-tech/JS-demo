239c4db6e9f99fcc8e3dd667063c28fb68e8744a
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-04 22:33:30 +0500

    Hoisting

--- Hoisting.js
+++ Hoisting.js
@@ -1,11 +1,23 @@
-let globalVar = "I am global";
+// let globalVar = "I am global";
 
-function testScope() {
-  let localVar = "I am local";
-  console.log(globalVar); // accessible
-  console.log(localVar);  // accessible
+// function testScope() {
+//   let localVar = "I am local";
+//   console.log(globalVar); // accessible
+//   console.log(localVar);  // accessible
+// }
+
+// testScope();
+// console.log(globalVar); // accessible
+// // console.log(localVar); // ❌ Error
+
+
+//Hoisting
+
+console.log(a); // undefined (var is hoisted)
+var a = 10;
+
+// Functions are fully hoisted
+greet();
+function greet() {
+  console.log("Hello!");
 }
-
-testScope();
-console.log(globalVar); // accessible
-// console.log(localVar); // ❌ Error
